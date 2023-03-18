fe8e4f3b52e1fb20c560a4c907d63b8301193e2a
Staneesh <blkreasons@outlook.com>
2023-03-19 00:32:04 +0100

    Add zellij and direnv

--- home.nix
+++ home.nix
@@ -2,8 +2,9 @@
 	programs.home-manager.enable = true;
 	programs.bash = {
 		enable = true;
+		# Hook direnv program to bash shell on shell startup
 		bashrcExtra = ''
-			eval "$(direnv hook bash)"
+			eval "$(direnv hook bash)" 
 		'';
 	};
 
@@ -11,7 +12,8 @@
 	home.username = "stanisz";
 	home.homeDirectory = "/home/stanisz";
 
-	home.packages = [
-		pkgs.direnv
+	home.packages = with pkgs; [
+		direnv		# Automatic shell.nix activation in programming environments
+		zellij		# Terminal multiplexter written in Rust
 	];
 }
